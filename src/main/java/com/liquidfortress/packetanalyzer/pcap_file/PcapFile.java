be8b08b93c8cadce6b93d5f42607509ce39482bc
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2017-11-29 16:01:21 -0800

    Stub for PcapFile

---
+++ src/main/java/com/liquidfortress/packetanalyzer/pcap_file/PcapFile.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2017.  Richard Scott McNew.
+ *
+ * This file is part of Liquid Fortress Packet Analyzer.
+ *
+ * Liquid Fortress Packet Analyzer is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Liquid Fortress Packet Analyzer is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Liquid Fortress Packet Analyzer.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.liquidfortress.packetanalyzer;
+
+import org.pcap4j.core.Pcaps;
+import org.pcap4j.core.PcapHandle;
+
+public class PcapFile {
+
+    public static PcapHandle openPcapFile(String filePath) {
+
+        return null;
+    }
+}
